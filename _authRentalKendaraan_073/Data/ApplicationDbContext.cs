6d50a8f21aab84a64696ed39230e5e34fe27022d
AdiatsaPutra <santikosetyo77@gmail.com>
2020-12-07 12:45:56 +0700

    Add project files.

---
+++ _authRentalKendaraan_073/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _authRentalKendaraan_073.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
